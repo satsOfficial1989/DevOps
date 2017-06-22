0601d52972782f213f66f6819d2a47ff3bf431f6
DC-2017 <pdutta@deloitte.com>
2017-06-22 17:04:38 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def sample():
-    return "<h2 style='color:green'>Welcome to DC CICD Demo - This is Change#1 </h2>"
+    return "<h2 style='color:green'>Welcome to Deloitte Cloud DevOps Demo - This is Change#1 </h2>"
     
 if __name__ == '__main__':
     app.run()

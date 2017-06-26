f451fb9e2054cebac68534848c8ce38ab6885786
DC-2017 <pdutta@deloitte.com>
2017-06-26 19:34:19 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def sample():
-    return "<h2 style='color:red'>Deloitte Cloud DevOps Demo Web Application</h2>"
+    return "<h1 style='color:red'>Deloitte Cloud DevOps Demo Web Application</h1>"
     
 if __name__ == '__main__':
     app.run()
